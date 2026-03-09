dd9ddb3673df53062d752ef093d199a08602c540
franugjo <m18236871@gmail.com>
2026-03-09 22:35:14 +0100

    Update basePath and add comments in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',      // To wymusza wygenerowanie statycznego HTML
-  basePath: '/xx',       // To mówi Next.js, że strona leży w podfolderze /xx/
+  // Wymusza wygenerowanie statycznych plików HTML/CSS/JS
+  output: 'export',
+  
+  // Informuje Next.js, że strona będzie pod adresem /xdddd/
+  basePath: '/xdddd',
+  
+  // Wyłącza optymalizację obrazów (wymagane na GitHub Pages)
   images: {
-    unoptimized: true,   // GitHub Pages nie obsługuje automatycznej optymalizacji zdjęć Next.js
+    unoptimized: true,
   },
 };
 
